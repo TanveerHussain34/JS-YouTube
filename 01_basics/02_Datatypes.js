e32f924fd7cc30856af62d31aff8bbd733519e2c
Tanveer Hussain <125460641+TanveerHussain34@users.noreply.github.com>
2024-07-09 09:18:54 +0000

    Datatypes

---
+++ 01_basics/02_Datatypes.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all JS code as newer version
+
+// age = 20 // using variable without declaration is not allowed in newer version of JS
+
+let age = 25 // limit of number datatype is 2^53-1, after that bigint is used
+
+let userName = "Tanveer"
+
+let isLoggedIn = true
+
+let identificationMark = null // object
+
+let state
+
+// symbol is another datatype used to determine uniqueness
+
+console.table([typeof age, typeof userName, typeof isLoggedIn, typeof identificationMark, typeof state, number]);
