e44b31fd6fdd722c166a8bacbe4a0b2d04db331b
Tanveer Hussain <125460641+TanveerHussain34@users.noreply.github.com>
2024-07-11 13:10:27 +0000

    Comparison Operators

---
+++ 01_basics/05_comparisonOperators.js
@@ -0,0 +1,33 @@
+console.log(2 > 4);
+console.log(2 >= 4);
+console.log(2 < 4);
+console.log(2 <= 4);
+console.log(2 == 4);
+console.log(2 != 4);
+
+// These are commonly used and already understood operators
+
+console.log("2" == 2);
+console.log(4 == "04");
+console.log("2" === 2);
+console.log(4 === "04");
+
+/* Here normal equality allows the comparison of a number and string while strict equality does not allow that */
+
+console.log(null < 0);
+console.log(null > 0);
+console.log(null == 0);
+console.log(null <= 0);
+console.log(null >= 0);
+console.log(null != 0);
+
+// This is the behavior of null with different values that must keep in mind
+
+console.log(undefined < 0);
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined <= 0);
+console.log(undefined >= 0);
+console.log(undefined != 0);
+
+// This is the behavior of undefined with different values that must keep in mind
