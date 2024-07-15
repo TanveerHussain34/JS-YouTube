016d06036a081f3493437195bee61aca226d3dd2
Tanveer Hussain <125460641+TanveerHussain34@users.noreply.github.com>
2024-07-15 16:00:41 +0000

    Object Declaration

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,34 @@
+/* Object Literals and Constructor Object(Singleton) are two types of object declaration, here we will discuss Object Literals */
+
+let mySym = Symbol("key1")
+
+let myObj = {
+    firstName: "Tanveer",
+    lastName: "Hussain",
+    "age": 23, // Possible because the compiler considers keys as strings
+    email: "maliktanveerhussain459@gmail.com",
+    Location: "Raiwind",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"],
+    [mySym]: "key1"
+}
+
+console.log(myObj)
+console.log(myObj.email)
+console.log(myObj["Location"])
+console.log(myObj[mySym]) // Accessing symbol from object
+
+Object.freeze(myObj)
+myObj.age = 23 // Will not work due to freeze()
+console.log(myObj)
+
+myObj.greeting = function(){
+    return "Hello Mister, Welcome!"
+}
+
+myObj.greetingTwo = function(){
+    return `Hello Mr. ${this.firstName} ${this.lastName}, Welcome!`
+}
+
+console.log(myObj.greeting())
+console.log(myObj.greetingTwo())
