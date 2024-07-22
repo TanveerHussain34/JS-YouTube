292172adc202287f91361a8699c4101ff59414ad
Tanveer Hussain <125460641+TanveerHussain34@users.noreply.github.com>
2024-07-22 23:57:33 +0500

    For In Loop

---
+++ 05_iterations/forIn.js
@@ -0,0 +1,47 @@
+// FOR IN LOOP
+
+// ["", "", ""]
+// [{}, {}, {}]
+
+// FOR IN LOOP ON ARRAY
+
+// let array = ["Pakistan", "Palestine", "China"];
+
+// for (const arr in array) {
+//   console.log(arr);
+// }
+
+// for (const arr in array) {
+//   console.log(array[arr]);
+// }
+
+// FOR IN LOOP ON STRING
+
+// let greetings = "Hello World!";
+
+// for (const greeting in greetings) {
+//   console.log(`Each char of ${greeting}`);
+// }
+
+// for (const greeting in greetings) {
+//   console.log(`Each char of ${greetings[greeting]}`);
+// }
+
+// FOR IN LOOP ON OBJECT
+
+// let friends = {
+//   f1: "Tanveer",
+//   f2: "Junaid",
+//   f3: "Aqib",
+//   f4: "Hashir",
+// };
+
+// for (const friend in friends) {
+//   console.log(friend);
+// }
+
+// for (const friend in friends) {
+//   console.log(friends[friend]);
+// }
+
+// FOR IN LOOP ON MAP DOESN'T WORK
