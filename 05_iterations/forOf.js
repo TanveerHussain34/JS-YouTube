7815e4136cda02d5458bae5b36047c32841e0ad1
Tanveer Hussain <125460641+TanveerHussain34@users.noreply.github.com>
2024-07-22 23:55:46 +0500

    For Of Loop in Detail

---
+++ 05_iterations/forOf.js
@@ -0,0 +1,49 @@
+// FOR OF LOOP
+
+// ["", "", ""]
+// [[], [], []]
+
+// FOR OF LOOP ON ARRAY
+
+// let array = ["Pakistan", "Palestine", "China"];
+
+// for (const arr of array) {
+//   console.log(arr);
+// }
+
+// FOR OF LOOP ON STRING
+
+// let greetings = "Hello World!";
+
+// for (const greeting of greetings) {
+//   console.log(`Each char of ${greeting}`);
+// }
+
+// FOR OF LOOP ON OBJECT
+
+// let friends = {
+//   f1: "Tanveer",
+//   f2: "Junaid",
+//   f3: "Aqib",
+//   f4: "Hashir",
+// };
+
+// for (const friend of Object.values(friends)) {
+//   console.log(friend);
+// }
+
+// MAP
+
+// const maps = new Map();
+// maps.set("PAK", "Pakistan");
+// maps.set("IND", "India");
+// maps.set("USA", "United States of America");
+// maps.set("PAK", "Pakistan");
+
+// for (const map of maps) {
+//   console.log(map);
+// }
+
+// for (const [key, value] of maps) {
+//   console.log(key, value);
+// }
